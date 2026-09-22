34fb0078cce8acdfd32c2721468b2f9cf1c5c9e2
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-22 17:44:53 -0600

    Fixed PieceMoveCalculator

--- shared/src/main/java/chess/PieceMoveCalculator.java
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -1,10 +1,7 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface PieceMoveCalculator {
-    static List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
-    }
+    List<ChessMove> pieceMovesGetter(ChessBoard board, ChessPosition myPosition);
 }
