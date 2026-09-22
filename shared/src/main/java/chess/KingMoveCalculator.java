a9b69b6db4130b8269bc1ad26c650898abe6f958
Jeffrey Hill <jeffreyhilljr@gmail.com>
2026-09-22 16:30:05 -0600

    Fixed KingMoveCalculator

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -13,8 +13,15 @@ public class KingMoveCalculator implements PieceMoveCalculator {
 
         for (int r = row - 1; r < row + 2; r++) {
             for (int c = col - 1; c < col + 2; c++) {
-                if(board.getPiece(new ChessPosition(r, c)) != null) {
-                    moves.add(new ChessMove(myPosition, new ChessPosition(r, c), null));
+                if (r >= 0 && r <= 8 && c >= 0 && c <= 8) {
+                    ChessPosition newPosition = new ChessPosition(r, c);
+                    if (board.getPiece(newPosition) == null) {
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    } else {
+                        if (board.getPiece(newPosition).getTeamColor() != team) {
+                            moves.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                    }
                 }
             }
         }
